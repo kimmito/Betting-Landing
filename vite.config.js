73b0cbccccdc5e5635e721427e356d5e49fff930
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2025-11-28 01:00:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Для GitHub Pages используйте '/ВАШЕ_НАЗВАНИЕ_РЕПОЗИТОРИЯ/'
-  // Если репозиторий называется test-seocare, оставьте как есть
-  // Для локальной разработки base будет '/'
-  base: process.env.GITHUB_ACTIONS ? '/test-seocare/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/Betting-Landing/' : '/',
   css: {
     preprocessorOptions: {
       scss: {
